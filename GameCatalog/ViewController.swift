7b831a0e9e3d26296951402a4be82419e56995df
Bryan A <bolivarbryan@gmail.com>
2019-01-03 17:37:52 -0500

    Initial Commit

---
+++ GameCatalog/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  GameCatalog
+//
+//  Created by Bryan A Bolivar M on 1/3/19.
+//  Copyright © 2019 Dev. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
